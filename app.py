22d43ebb4824e8c03758d622361aeab72d5d084d
YASH4-HD <msicc14@gmail.com>
2026-02-11 17:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,13 +101,16 @@ def inject_responsive_design():
 
     </style>
 
-    <div aria-hidden="true">
-      <div class="particle" style="left:10%; animation-delay:0s;">🧬</div>
-      <div class="particle" style="left:35%; animation-delay:5s;">●</div>
-      <div class="particle" style="left:60%; animation-delay:2s;">○</div>
-      <div class="particle" style="left:85%; animation-delay:8s;">🧬</div>
-    </div>
-    """, unsafe_allow_html=True)
+    # Wrap the HTML in st.markdown so it runs as code, not text
+st.markdown("""
+<div aria-hidden="true">
+    <div class="particle" style="left:10%; animation-delay:0s;">🧬</div>
+    <div class="particle" style="left:35%; animation-delay:5s;">●</div>
+    <div class="particle" style="left:60%; animation-delay:2s;">○</div>
+    <div class="particle" style="left:85%; animation-delay:8s;">🧬</div>
+</div>
+""", unsafe_allow_html=True)
+
 
 inject_responsive_design()
 
